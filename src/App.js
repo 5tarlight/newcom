6817a4cf2a82107029db04eb8153a2635d6eb35b
5tarlight <developerstarlight@gmail.com>
2019-07-27 22:31:39 +0900

    life cycle method practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample'
+import LifeCycle from './LifeCycleSample'
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
+
   render() {
     return (
-      <IterationSample />
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycle color={this.state.color}/>
+      </div>
     )
   }
 }
